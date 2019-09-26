1b9a0a020a35cbd769567075b1073a1eddc6c90f
k-mac-11 <kmac112233445566@gmail.com>
2019-09-26 13:57:39 +0000

    bridge.clj is work in progress

--- src/first_steps/bridge.clj
+++ src/first_steps/bridge.clj
@@ -1,11 +1,55 @@
-;start torch is 1 and state is [1, 1, 1, 1] and time is [t1, t2, t3, t4]
-;goal is torch is 0 and state is [0, 0, 0, 0]
-;while not (torch = 0 and state = [0, 0, 0, 0])
-;if torch is 1 then torch goes to 0 and generate all possible two positions of state go from 1 to 0. say position i and j. time taken is the max of ti and tj.
-;if torch is 0 then torch goes to 1 and one position of state goes from 0 to 1. say position i. time taken is ti.
+(def initial {:t 0 :p1 0 :p2 0 :p3 0 :p4 0})
 
-time taken is max of the ti's
+(def goal {:t 1 :p1 1 :p2 1 :p3 1 :p4 1})
 
-(defn next-move (state)
-  (if (> torch 0)
-    ))
+(def all-states (for [y [0 1] x1 [0 1] x2 [0 1] x3 [0 1] x4 [0 1]]
+                  {:t y :p1 x1 :p2 x2 :p3 x3 :p4 x4}))
+
+(defn torch-0-1 [state1 state2]
+  (and (= (:t state1) 0) (= (:t state2) 1)))
+
+(defn torch-1-0 [state1 state2]
+  (and (= (:t state1) 1) (= (:t state2) 0)))
+
+(defn two-ps-0-1 [state1 state2]
+  (let [diff (map - (rest (vals state2)) (rest (vals state1)))
+        pluses (filter pos? diff)
+        minuses (filter neg? diff)]    
+    (if (and (= (count pluses) 2) (empty? minuses))
+      true
+      false)))
+
+(defn one-p-1-0 [state1 state2]
+  (let [diff (map - (rest (vals state2)) (rest (vals state1)))
+        pluses (filter pos? diff)
+        minuses (filter neg? diff)]    
+    (if (and (empty? pluses) (= (count minuses) 1))
+      true
+      false)))
+
+(defn possible-state [state1 state2]
+  (if (or (and (torch-0-1 state1 state2) (two-ps-0-1 state1 state2))
+          (and (torch-1-0 state1 state2) (one-p-1-0 state1 state2)))
+    true
+    false))
+
+(defn all-possible-states [state all-states]
+  (remove nil?
+   (for [x (range (count all-states))]
+    (if (possible-state state (nth all-states x))
+      (nth all-states x)))))
+
+(defn build-graph [state all-states]
+  (loop [current state
+         visited #{current}
+         neighbors (into [] (all-possible-states current all-states))
+         to-visit neighbors
+         graph [{:node current :neighbors neighbors}]]
+    (if (empty? to-visit)
+      graph      
+      (let [next (peek to-visit)
+            new-visited (conj visited next)
+            new-neighbors (into [] (all-possible-states next all-states))
+            new-to-visit (into (pop to-visit) (remove visited new-neighbors))
+            new-graph (conj graph {:node next :neighbors new-neighbors})]
+        (recur next new-visited new-neighbors new-to-visit new-graph)))))
